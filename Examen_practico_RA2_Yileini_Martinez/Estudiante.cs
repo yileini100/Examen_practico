d2b4de0c37e6bd1bd87562721d338b3c5522e204
yileini100 <153277863+yileini100@users.noreply.github.com>
2024-03-05 23:51:45 -0400

    Add files via upload

---
+++ Examen_practico_RA2_Yileini_Martinez/Estudiante.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_practico_RA2_Yileini_Martinez
+{
+    internal class Estudiante
+    {
+      
+            // Propiedades del estudiante
+            public string Matricula { get; set; }
+            public string Nombre { get; set; }
+            public string Direccion { get; set; }
+            public double Telefono { get; set; }
+            public string Email { get; set; }
+            public string Maestro_titular { get; set; }
+
+       
+            
+        
+    }
+}
